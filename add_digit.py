93c5baf2e1eb80d6bc422ea0036b8add0f95ac63
armelBla <110736523+armelBla@users.noreply.github.com>
2022-12-09 17:35:32 -0500

    Add file day 2

---
+++ add_digit.py
@@ -0,0 +1,8 @@
+two_digit_number = input("Type a two digit number: ")
+
+first_number = int(two_digit_number[0])
+last_number = int(two_digit_number[-1])
+
+add_number = first_number + last_number
+
+print(f"the total of two digit number is {add_number}")
